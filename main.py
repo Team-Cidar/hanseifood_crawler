ab8c5a167d7c226e4148a5c50e6e134981a5cc89
Hoooooou-Jun <jipkim2@gmail.com>
2023-06-03 18:38:20 +0900

    주간식단표_본관 접근, 번호 테이블 요소 선택까지만..

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
 import pickle
 
 
@@ -11,12 +10,22 @@ def crawling():
     ...
 
 
+# Chrome Driver Path, Options
+path = "./drivers/chromedriver"
 options = webdriver.ChromeOptions()
 options.add_experimental_option("prefs", {'intl.accept_languages': 'ko'})  # driver language setting
 
-driver = webdriver.Chrome(executable_path="./drivers/chromedriver", options=options)
-driver.get("https://www.naver.com")
+# Chrome Driver
+driver = webdriver.Chrome(executable_path=path, options=options)
 
+# Logic
+driver.get("https://portal.hansei.ac.kr/portal/default/gnb/hanseiTidings/weekMenuTable.page")
+
+tableNumberElement = "first C"
+print(driver.find_elements(By.CLASS_NAME, tableNumberElement))
+
+
+# 종료 트리거
 while True:
     if input() == "q":
         driver.close()
